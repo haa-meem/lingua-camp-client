4134a793b13605bb367a61f5fbfdabed7381b26e
Haa-meem <haameemalhamra@gmail.com>
2023-06-09 22:53:05 +0600

    data loaded from server

--- src/Pages/Home/PopularClasses/PopularClasses.jsx
+++ src/Pages/Home/PopularClasses/PopularClasses.jsx
@@ -4,7 +4,7 @@ const PopularClasses = () => {
     const [classes, setClasses] = useState([]);
 
     useEffect(() => {
-        fetch('classes.json')
+        fetch('http://localhost:5000/classes')
             .then((res) => res.json())
             .then((data) => setClasses(data));
     }, []);
--- src/Pages/Home/PopularInstructors/PopularInstructors.jsx
+++ src/Pages/Home/PopularInstructors/PopularInstructors.jsx
@@ -4,7 +4,7 @@ const PopularInstructors = () => {
   const [instructors, setInstructors] = useState([]);
 
   useEffect(() => {
-    fetch('instructors.json')
+    fetch('http://localhost:5000/instructors')
       .then(res => res.json())
       .then(data => setInstructors(data.slice(0, 6)))
   }, [])
