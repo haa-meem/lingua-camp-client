653f26cdd610d9bae2ee5d7ccc81d5726479cbc0
Haa-meem <haameemalhamra@gmail.com>
2023-06-09 14:42:02 +0600

    PopularClasses and PopularClasses mapped from json

--- src/Pages/Home/PopularClasses/PopularClasses.jsx
+++ src/Pages/Home/PopularClasses/PopularClasses.jsx
@@ -1,92 +1,32 @@
-import pic1 from "../../../assets/pic1.jpg"
-import pic2 from "../../../assets/pic2.jpg"
-import pic3 from "../../../assets/pic3.jpg"
-import pic4 from "../../../assets/pic4.jpg"
-import pic5 from "../../../assets/pic5.jpg"
-import pic6 from "../../../assets/pic6.jpg"
+import { useEffect, useState } from 'react';
 
 const PopularClasses = () => {
+    const [classes, setClasses] = useState([]);
+
+    useEffect(() => {
+        fetch('classes.json')
+            .then((res) => res.json())
+            .then((data) => setClasses(data));
+    }, []);
+
     return (
         <>
-            <h1 className='text-center font-bold text-5xl my-3'>Popular Classes</h1>
+            <h1 className="text-center font-bold text-5xl my-3">Popular Classes</h1>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 m-3">
-                <div className="card w-full bg-base-100 shadow-xl">
-                    <figure className="px-3 pt-3">
-                        <img src={pic1} alt="Spanish Language Class" className="rounded-xl" />
-                    </figure>
-                    <div className="card-body items-center text-center">
-                        <h2 className="card-title">Spanish Language Class</h2>
-                        <p>This class focuses on teaching students the fundamentals of the Spanish language through interactive activities, conversations, and cultural exploration.</p>
-                        <div className="card-actions">
-                            <button className="btn btn-primary">Learn More</button>
+                {classes.map((classItem) => (
+                    <div className="card w-full bg-base-100 shadow-xl" key={classItem.id}>
+                        <figure className="px-10 pt-10">
+                            <img src={classItem.image} alt={classItem.title} className="rounded-xl" />
+                        </figure>
+                        <div className="card-body items-center text-center">
+                            <h2 className="card-title">{classItem.title}</h2>
+                            <p>{classItem.description}</p>
+                            <div className="card-actions">
+                                <button className="btn btn-primary">Learn More</button>
+                            </div>
                         </div>
                     </div>
-                </div>
-
-                <div className="card w-full bg-base-100 shadow-xl">
-                    <figure className="px-3 pt-3">
-                        <img src={pic2} alt="French Language Class" className="rounded-xl" />
-                    </figure>
-                    <div className="card-body items-center text-center">
-                        <h2 className="card-title">French Language Class</h2>
-                        <p>In this class, students will immerse themselves in the beauty of the French language. They will learn vocabulary, grammar, and pronunciation while exploring French culture and traditions.</p>
-                        <div className="card-actions">
-                            <button className="btn btn-primary">Learn More</button>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="card w-full bg-base-100 shadow-xl">
-                    <figure className="px-3 pt-3">
-                        <img src={pic3} alt="German Language Class" className="rounded-xl" />
-                    </figure>
-                    <div className="card-body items-center text-center">
-                        <h2 className="card-title">German Language Class</h2>
-                        <p>Join our German Language Class to dive into the intricacies of the German language. Students will develop their speaking, reading, and writing skills while engaging in fun and engaging activities.</p>
-                        <div className="card-actions">
-                            <button className="btn btn-primary">Learn More</button>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="card w-full bg-base-100 shadow-xl">
-                    <figure className="px-3 pt-3">
-                        <img src={pic4} alt="Chinese Language Class" className="rounded-xl" />
-                    </figure>
-                    <div className="card-body items-center text-center">
-                        <h2 className="card-title">Chinese Language Class</h2>
-                        <p>Discover the wonders of the Chinese language and its rich cultural heritage. This class will introduce students to Chinese characters, basic conversational skills, and traditional customs.</p>
-                        <div className="card-actions">
-                            <button className="btn btn-primary">Learn More</button>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="card w-full bg-base-100 shadow-xl">
-                    <figure className="px-3 pt-3">
-                        <img src={pic5} alt="Arabic Language Class" className="rounded-xl" />
-                    </figure>
-                    <div className="card-body items-center text-center">
-                        <h2 className="card-title">Arabic Language Class</h2>
-                        <p>Immerse yourself in the captivating world of the Arabic language and culture. Students will learn Arabic alphabet, basic vocabulary, and essential phrases.</p>
-                        <div className="card-actions">
-                            <button className="btn btn-primary">Learn More</button>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="card w-full bg-base-100 shadow-xl">
-                    <figure className="px-3 pt-3">
-                        <img src={pic6} alt="Polish Language Class" className="rounded-xl" />
-                    </figure>
-                    <div className="card-body items-center text-center">
-                        <h2 className="card-title">Polish Language Class</h2>
-                        <p>Embark on a linguistic and cultural journey through Poland. This class will teach students the basics of Polish grammar, vocabulary, and pronunciation, while also exploring Polish traditions and customs.</p>
-                        <div className="card-actions">
-                            <button className="btn btn-primary">Learn More</button>
-                        </div>
-                    </div>
-                </div>
+                ))}
             </div>
         </>
     );
--- src/Pages/Home/PopularInstructors/PopularInstructors.jsx
+++ src/Pages/Home/PopularInstructors/PopularInstructors.jsx
@@ -1,95 +1,35 @@
-import person1 from "../../../assets/person1.png"
-import person2 from "../../../assets/person2.png"
-import person3 from "../../../assets/person3.png"
-import person4 from "../../../assets/person4.png"
-import person5 from "../../../assets/person5.png"
-import person6 from "../../../assets/person6.png"
+import { useEffect, useState } from 'react';
 
 const PopularInstructors = () => {
-    return (
-        <>
-            <h1 className='text-center font-bold text-5xl my-3'>Popular Instructors</h1>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 m-3">
-                <div className="card w-full bg-base-100 shadow-xl rounded-full">
-                    <figure className="w-24 mx-auto mt-5">
-                        <img src={person1} alt="Instructor 1" className="rounded-xl" />
-                    </figure>
-                    <div className="card-body items-center text-center">
-                        <h2 className="card-title">Instructor 1</h2>
-                        <p>Teaching Spanish</p>
-                        <div className="card-actions">
-                            <button className="btn btn-primary">Learn More</button>
-                        </div>
-                    </div>
-                </div>
+  const [instructors, setInstructors] = useState([]);
 
-                <div className="card w-full bg-base-100 shadow-xl rounded-full">
-                    <figure className="w-24 mx-auto mt-5">
-                        <img src={person2} alt="Instructor 2" className="rounded-xl" />
-                    </figure>
-                    <div className="card-body items-center text-center">
-                        <h2 className="card-title">Instructor 2</h2>
-                        <p>Teaching French</p>
-                        <div className="card-actions">
-                            <button className="btn btn-primary">Learn More</button>
-                        </div>
-                    </div>
-                </div>
+  useEffect(() => {
+    fetch('instructors.json')
+        .then(res => res.json())
+        .then(data => setInstructors(data))
+}, [])
 
-                <div className="card w-full bg-base-100 shadow-xl rounded-full">
-                    <figure className="w-24 mx-auto mt-5">
-                        <img src={person3} alt="Instructor 3" className="rounded-xl" />
-                    </figure>
-                    <div className="card-body items-center text-center">
-                        <h2 className="card-title">Instructor 3</h2>
-                        <p>Teaching German</p>
-                        <div className="card-actions">
-                            <button className="btn btn-primary">Learn More</button>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="card w-full bg-base-100 shadow-xl rounded-full">
-                    <figure className="w-24 mx-auto mt-5">
-                        <img src={person4} alt="Instructor 4" className="rounded-xl" />
-                    </figure>
-                    <div className="card-body items-center text-center">
-                        <h2 className="card-title">Instructor 4</h2>
-                        <p>Teaching Chinese</p>
-                        <div className="card-actions">
-                            <button className="btn btn-primary">Learn More</button>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="card w-full bg-base-100 shadow-xl rounded-full">
-                    <figure className="w-24 mx-auto mt-5">
-                        <img src={person5} alt="Instructor 5" className="rounded-xl" />
-                    </figure>
-                    <div className="card-body items-center text-center">
-                        <h2 className="card-title">Instructor 5</h2>
-                        <p>Teaching Arabic</p>
-                        <div className="card-actions">
-                            <button className="btn btn-primary">Learn More</button>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="card w-full bg-base-100 shadow-xl rounded-full">
-                    <figure className="w-24 mx-auto mt-5">
-                        <img src={person6} alt="Instructor 6" className="rounded-xl" />
-                    </figure>
-                    <div className="card-body items-center text-center">
-                        <h2 className="card-title">Instructor 6</h2>
-                        <p>Teaching Polish</p>
-                        <div className="card-actions">
-                            <button className="btn btn-primary">Learn More</button>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <>
+      <h1 className="text-center font-bold text-5xl my-3">Popular Instructors</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 m-3">
+        {instructors.map((instructor) => (
+          <div className="card w-full bg-base-100 shadow-xl rounded-full" key={instructor.id}>
+            <figure className="w-24 mx-auto mt-5">
+              <img src={instructor.image} alt={instructor.name} className="rounded-xl" />
+            </figure>
+            <div className="card-body items-center text-center">
+              <h2 className="card-title">{instructor.name}</h2>
+              <p>{instructor.teaching}</p>
+              <div className="card-actions">
+                <button className="btn btn-primary">Learn More</button>
+              </div>
             </div>
-        </>
-    )
+          </div>
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default PopularInstructors;
