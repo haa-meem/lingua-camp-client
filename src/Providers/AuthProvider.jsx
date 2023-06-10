c6baad87fbeeabfedff6b0806e4d5c2c64025978
Haa-meem <haameemalhamra@gmail.com>
2023-06-11 02:16:36 +0600

    jwt set and remove

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut, updateProfile } from "firebase/auth";
 import app from '../Firebase/firebase.config';
+import axios from "axios";
 
 export const AuthContext = createContext(null);
 
@@ -34,6 +35,19 @@ const AuthProvider = ({ children }) => {
         const unsubscribe = onAuthStateChanged(auth, currentUser => {
             console.log('logged in user inside auth state observer', currentUser)
             setUser(currentUser);
+
+            //token
+            if(currentUser){
+                axios.post('http://localhost:5000/jwt',{email:currentUser.email})
+                .then(data=>{
+                    console.log(data.data.token);
+                    localStorage.setItem('access-token',data.data.token)
+                })
+            }
+            else{
+                localStorage.removeItem('access-token')
+            } 
+
             setLoading(false);
         })
         return () => {
