4e136483517b61ceba2a34f0dda525c92ed679ff
Haa-meem <haameemalhamra@gmail.com>
2023-06-11 02:38:12 +0600

    verify jwt

--- src/Hooks/useEnroll.jsx
+++ src/Hooks/useEnroll.jsx
@@ -4,11 +4,16 @@ import { AuthContext } from '../Providers/AuthProvider';
 
 const useEnroll = () => {
     const { user } = useContext(AuthContext);
+    const token = localStorage.getItem('access-token');
 
     const { refetch, data: enroll = [] } = useQuery({
         queryKey: ['enrolled', user?.email],
         queryFn: async () => {
-            const res = await fetch(`http://localhost:5000/enrolled?email=${user.email}`)
+            const res = await fetch(`http://localhost:5000/enrolled?email=${user.email}`, {
+                headers: {
+                    authorization: `bearer ${token}`
+                }
+            })
             return res.json();
         }
     })
