0bac5e55baf2f36b9f7c72187eb0ef5cfca12890
Haa-meem <haameemalhamra@gmail.com>
2023-06-12 12:59:29 +0600

    made instructor

--- src/Pages/Dashboard/ManageUsers/ManageUsers.jsx
+++ src/Pages/Dashboard/ManageUsers/ManageUsers.jsx
@@ -1,68 +1,103 @@
 import { useQuery } from "@tanstack/react-query";
-import { FaTrashAlt, FaUserShield } from "react-icons/fa";
+import { FaUserShield, FaChalkboardTeacher } from "react-icons/fa";
 import Swal from "sweetalert2";
 import useAxiosSecure from "../../../Hooks/useAxiosSecure";
 
 const ManageUsers = () => {
-    const [axiosSecure]=useAxiosSecure();
-    const { data: users = [], refetch } = useQuery(['users'], async () => {
-        const res = await axiosSecure.get('/users')
+    const [axiosSecure] = useAxiosSecure();
+    const { data: users = [], refetch } = useQuery(["users"], async () => {
+        const res = await axiosSecure.get("/users");
         return res.data;
-    })
+    });
 
-    const handleMakeAdmin = user => {
+    const handleMakeAdmin = (user) => {
         fetch(`http://localhost:5000/users/admin/${user._id}`, {
-            method: 'PATCH'
+            method: "PATCH",
         })
-            .then(res => res.json())
-            .then(data => {
-                console.log(data)
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data);
                 if (data.modifiedCount) {
                     refetch();
                     Swal.fire({
-                        position: 'top-end',
-                        icon: 'success',
+                        position: "top-end",
+                        icon: "success",
                         title: `${user.name} is an Admin Now!`,
                         showConfirmButton: false,
-                        timer: 1500
-                    })
+                        timer: 1500,
+                    });
                 }
-            })
-    }
+            });
+    };
 
-    const handleDelete = user => {
-
-    }
+    const handleMakeInstructor = (user) => {
+        fetch(`http://localhost:5000/users/instructor/${user._id}`, {
+            method: "PATCH",
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data);
+                if (data.modifiedCount) {
+                    refetch();
+                    Swal.fire({
+                        position: "top-end",
+                        icon: "success",
+                        title: `${user.name} is an Instructor Now!`,
+                        showConfirmButton: false,
+                        timer: 1500,
+                    });
+                }
+            });
+    };
 
     return (
-        <div className="w-full">
-            <h2 className="text-3xl font-semibold my-4">All Users:{users.length}</h2>
-            <div className="overflow-x-auto">
-                <table className="table table-zebra">
-                    {/* head */}
-                    <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>Name</th>
-                            <th>Email</th>
-                            <th>Role</th>
-                            <th>Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            users.map((user, index) => <tr key={user._id}>
-                                <th>{index + 1}</th>
-                                <td>{user.name}</td>
-                                <td>{user.email}</td>
-                                <td>{user.role === 'admin' ? 'admin' :
-                                    <button onClick={() => handleMakeAdmin(user)} className="btn btn-ghost bg-orange-600 text-white"><FaUserShield></FaUserShield></button>
-                                }</td>
-                                <td><button onClick={() => handleDelete(user)} className="btn btn-ghost bg-red-600 text-white"><FaTrashAlt></FaTrashAlt></button></td>
-                            </tr>)
-                        }
-                    </tbody>
-                </table>
+        <div className="flex justify-center items-center h-screen">
+            <div className="w-full">
+                <h2 className="text-3xl font-semibold my-4 text-center">Manage Users: {users.length}</h2>
+                <div className="overflow-x-auto">
+                    <table className="table table-zebra">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Name</th>
+                                <th>Email</th>
+                                <th>Role</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {users.map((user, index) => (
+                                <tr key={user._id}>
+                                    <th>{index + 1}</th>
+                                    <td>{user.name}</td>
+                                    <td>{user.email}</td>
+                                    <td>
+                                        {user.role === "admin" ? (
+                                            "admin"
+                                        ) : user.role === "instructor" ? (
+                                            "instructor"
+                                        ) : (
+                                            <>
+                                                <button
+                                                    onClick={() => handleMakeInstructor(user)}
+                                                    className="btn btn-ghost bg-green-600 text-white me-2"
+                                                >
+                                                    <FaChalkboardTeacher />
+                                                </button>
+                                                <button
+                                                    onClick={() => handleMakeAdmin(user)}
+                                                    className="btn btn-ghost bg-orange-600 text-white"
+                                                >
+                                                    <FaUserShield />
+                                                </button>
+                                            </>
+                                        )}
+                                    </td>
+                                    <td></td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     );
